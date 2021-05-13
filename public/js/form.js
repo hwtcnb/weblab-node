87414d62b8eb0bbe6b21a20a1083699571871c8d
hwtcnb <hwtcnb@gmail.com>
2021-05-13 16:33:21 +0300

    edit

--- public/js/form.js
+++ public/js/form.js
@@ -1,9 +1,9 @@
 function openPopup(element){
-    document.querySelector(element).style.left = '100px'
+    document.querySelector(element).style.left = '70px';
 };
 
 function closePopup(element){
-   document.querySelector(element).style.left = '-9999px'
+   document.querySelector(element).style.left = '-9999px';
 };
 document.querySelector('.dialog-button').onclick = () => {
    openPopup('#popup')
@@ -13,12 +13,12 @@ document.querySelector('#close').onclick = () => {
    closePopup('#popup')
 }
 
-const select = document.querySelector('#selector')
-const now = new Date()
+const select = document.querySelector('#selector');
+const now = new Date();
 
 for (let i = 2000; i <= now.getFullYear(); i++) {
-   let options = String(i)
-   let el = document.createElement('option')
+   let options = String(i);
+   let el = document.createElement('option');
    el.textContent = options
    el.value = options
    select.appendChild(el) 
