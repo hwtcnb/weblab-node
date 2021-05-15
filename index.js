a493cdb2a2b8f1461d18154d1ba499cf16f64a11
hwtcnb <hwtcnb@gmail.com>
2021-05-15 22:25:21 +0300

    edit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const pool = require("./db");
 
 
 const PORT = process.env.PORT || 5000;
@@ -11,16 +12,25 @@ app.use(express.json())
 
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.get("/",function(request, response){
+app.get("/", (request, response) => {
     response.sendFile(__dirname + "/public/html/index.html");
 })
 
-app.post("/form", function (request, response) {
+app.post("/form", async(request, response) => {
     if(!request.body) return response.sendStatus(400);
+    let {name, surname, email, password, sex, year} = request.body;
+    if (!sex) {
+        sex = "no choose";
+    }
+    const newUser = await pool.query(
+        "INSERT INTO users (name, surname, email, password, sex, year) VALUES($1, $2, $3, $4, $5, $6)",
+        [name, surname, email, password, sex, year]
+    )
     response.json(request.body); 
+
 });
   
-app.get("/form",function(request, response){
+app.get("/form", (request, response) => {
     response.sendFile(__dirname + "/public/html/form.html");
 });
 
---
+++ mydb.sql
@@ -0,0 +1,9 @@
+CREATE TABLE users(
+    user_id SERIAL PRIMARY KEY,
+    name VARCHAR (25),
+    surname VARCHAR (25),
+    email VARCHAR (25),
+    password VARCHAR (25),
+    sex VARCHAR (25),
+    year VARCHAR (25)
+);
