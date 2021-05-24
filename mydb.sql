3c455743f9ddd93b9b75186049e69b5d27fa4cb4
hwtcnb <hwtcnb@gmail.com>
2021-05-24 16:30:45 +0300

    edit

--- mydb.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE users(
-    user_id SERIAL PRIMARY KEY,
-    name VARCHAR (25),
-    surname VARCHAR (25),
-    email VARCHAR (25),
-    password VARCHAR (25),
-    sex VARCHAR (25),
-    year VARCHAR (25)
-);
